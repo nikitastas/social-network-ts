d7072c46fda7c1093a6513ac315c5358f74749cb
nikitastas <nikitastas1694@gmail.com>
2024-06-02 22:07:56 +0300

    Add Header and Technologies components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,31 @@ import './App.css';
 function App() {
     return (
         <div>
-            <div>one div</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
-            </div>
+            <Header />
+            <Technologies />
         </div>
     );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (<div>
+        <a href="#s">Home </a>
+        <a href="#s">News Feed </a>
+        <a href="#s">Messages</a>
+    </div>);
 }
 
 export default App;
