14da3f0b6c0e1a5e2c70f828370bf3f1954eb889
nikitastas <nikitastas1694@gmail.com>
2024-06-03 14:42:31 +0300

    Improved the layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,44 @@ function App() {
                 <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712966400&semt=ais" alt=""/>
             </header>
             <nav className='nav'>
-                <div>Profile</div>
-                <div>Messages</div>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
             </nav>
-            <div className='content'>Main content</div>
+            <div className='content'>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
