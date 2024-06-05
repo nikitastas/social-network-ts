44b879cf7ceae509a586b4a780cd006ac245163f
nikitastas <nikitastas1694@gmail.com>
2024-06-05 09:58:52 +0300

    Add props to Post component

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+type PostType = {
+    message: string;
+}
+
+const Post = (props: PostType) => {
     return (
         <div className={s.posts}>
             <div className={s.item}>
                 <img
                     src="https://s13.stc.all.kpcdn.net/afisha/msk/wp-content/uploads/sites/5/2023/01/kadr-iz-filma-avatar-put-vody.jpg"
                     alt=""/>
-                post 1
+                {props.message}
                 <div>
                     <span>like</span>
                 </div>
