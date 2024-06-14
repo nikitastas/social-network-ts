fa8e158275d6b4efb1c469f12070b614076a4cdd
nikitastas <nikitastas1694@gmail.com>
2024-06-15 01:08:17 +0300

    Complete 20 stage

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import s from './Navbar.module.css';
 
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to={'/profile'} >Profile</NavLink>
+                <NavLink to={'/profile'}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
                 <NavLink to={'/dialogs'}>Messages</NavLink>
