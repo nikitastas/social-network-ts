3cf428531179316695f4eb1699ee01d940516e4c
nikitastas <nikitastas1694@gmail.com>
2024-06-16 01:55:59 +0300

    Complete 21 stage

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Nikita
+                </div>
+                <div className={s.dialog}>
+                    Yuriy
+                </div>
+                <div className={s.dialog}>
+                    Ilya
+                </div>
+                <div className={s.dialog}>
+                    Evgeniy
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
