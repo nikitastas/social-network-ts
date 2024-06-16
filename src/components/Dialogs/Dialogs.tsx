b8ce8df044b494bdcb28c3dfbedec9a3d11328eb
nikitastas <nikitastas1694@gmail.com>
2024-06-17 01:23:33 +0300

    Moved DialogItem into a separate component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from 'react-router-dom';
 
+type Props = {
+    id: number
+    name: string
+}
+
+const DialogItem = ({id, name}: Props) => {
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={'/dialogs/' + id}>{name}</NavLink>
+        </div>
+    )
+}
+
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to={'/dialogs/1'}>Nikita</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/2'}>Yuriy</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/3'}>Ilya</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/4'}>Evgeniy</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/5'}>Alex</NavLink>
-                </div>
+                <DialogItem id={1} name={'Nikita'}/>
+                <DialogItem id={2} name={'Ilya'}/>
+                <DialogItem id={3} name={'Evgeniy'}/>
+                <DialogItem id={4} name={'Alex'}/>
+                <DialogItem id={5} name={'Yuriy'}/>
+                <DialogItem id={6} name={'Roman'}/>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
