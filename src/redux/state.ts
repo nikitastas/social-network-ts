3a50d6663160ce28eb489cab7f50d02ee8b2dbbe
nikitastas <nikitastas1694@gmail.com>
2024-08-06 23:49:52 +0300

    Added store.

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,3 +1,35 @@
+let store = {
+    state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi, how are you?", likesCount: 12},
+                {id: 2, message: "It's my first post", likesCount: 11},
+                {id: 2, message: "Bla bla", likesCount: 9},
+                {id: 2, message: "Da da", likesCount: 19},
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Nikita'},
+                {id: 2, name: 'Ilya'},
+                {id: 3, name: 'Evgeniy'},
+                {id: 4, name: 'Alex'},
+                {id: 5, name: 'Yuriy'},
+                {id: 6, name: 'Roman'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'},
+                {id: 6, message: 'Yo'},
+            ],
+        },
+    }
+}
+
 let rerenderEntireTree = (state: StateType) => {
     console.log('State changed')
 }
