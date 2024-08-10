c530dc05942a6cb8a96ed913c4155d3abec1b9bf
nikitastas <nikitastas1694@gmail.com>
2024-08-10 23:51:25 +0300

    Added dispatch method in store.

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -34,23 +34,28 @@ export let store = {
     _callSubscriber(state: StateType) {
         console.log('State changed')
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        }
-        this._state.profilePage.posts.push(newPost)
-        this._state.profilePage.newPostText = ''
-        this._callSubscriber(this._state)
-    },
-    updateNewPostText(newText: string) {
-        this._state.profilePage.newPostText = newText
-        this._callSubscriber(this._state)
-    },
     subscribe(observer: (state: StateType) => void) {
         this._callSubscriber = observer
-    }
+    },
+    dispatch(action: any) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0,
+            }
+            this._state.profilePage.posts.push(newPost)
+            this._state.profilePage.newPostText = ''
+            this._callSubscriber(this._state)
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = newText
+            this._callSubscriber(this._state)
+        }
+    },
+    updateNewPostText(newText: string) {
+
+    },
+
 }
 
 
