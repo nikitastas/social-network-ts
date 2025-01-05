a3cf7b512d88d824864b2f0f766336d6be2a28ce
nikitastas <nikitastas1694@gmail.com>
2025-01-05 23:10:46 +0300

    Complete 53 stage.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -30,62 +30,37 @@ type Props = {
     setUsers: (users: UserType[]) => void
 }
 
-export const Users = ({users, follow, unfollow, setUsers}: Props) => {
+export class Users extends React.Component<Props> {
+	constructor(props: Props) {
+		super(props)
 
-	const getUsers = () => {
-		if (users.length === 0) {
+		if (this.props.users.length === 0) {
 			axios.get<UsersResponseType>('https://social-network.samuraijs.com/api/1.0/users').then(res => {
 				console.log(res)
-				setUsers(res.data.items)
+				this.props.setUsers(res.data.items)
 			})
 		}
 	}
 
-    /*useEffect(() => {
-        setUsers([
-            {
-                id: v1(),
-                photoUrl: 'https://cdn.7days.ru/pic/f68/1004510/1597471/86.jpg',
-                followed: false,
-                fullName: 'Nikita',
-                status: 'developer',
-                location: {city: 'Grodno', country: 'Belarus'}
-            },
-            {
-                id: v1(),
-                photoUrl: 'https://cdn.7days.ru/pic/f68/1004510/1597471/86.jpg',
-                followed: true,
-                fullName: 'Andrew',
-                status: 'I am a boss',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: v1(),
-                photoUrl: 'https://cdn.7days.ru/pic/f68/1004510/1597471/86.jpg',
-                followed: false,
-                fullName: 'Evgeny',
-                status: 'developer',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-        ])
-    }, [])*/
+	getUsers = () => {
 
-    return (
-        <div>
-			<button onClick={getUsers}>Get Users</button>
-            {
-                users.map(u => <div key={u.id}>
+	}
+    render() {
+		return (
+			<div>
+				{
+					this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
-                                : <button onClick={() => follow(u.id)}>Follow</button>}
+								? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+								: <button onClick={() => this.props.follow(u.id)}>Follow</button>}
                         </div>
                     </span>
-                    <span>
+						<span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -95,8 +70,9 @@ export const Users = ({users, follow, unfollow, setUsers}: Props) => {
                             <div>{'u.location.city'}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    )
+					</div>)
+				}
+			</div>
+		)
+	}
 }
