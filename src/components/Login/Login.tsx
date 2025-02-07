02e834828799cf5b032a32e65d9cbcccd0ffe60a
nikitastas <nikitastas1694@gmail.com>
2025-02-07 21:42:42 +0300

    Complete 75 stage.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,117 @@
-import React from 'react'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import FormLabel from '@mui/material/FormLabel'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import s from './Login.module.css'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router'
+import {AppThunkDispatch} from "../../redux/redux-store";
+
+type Inputs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 
 export const Login = () => {
+  debugger
+  //const isLoggedIn = useAppSelector(selectIsLoggedIn)
+  const dispatch = useDispatch<AppThunkDispatch>()
+
+  //const navigate = useNavigate()
+
+  /*useEffect(() => {
+    if (isLoggedIn) {
+      navigate(Path.Main)
+    }
+  }, [isLoggedIn])*/
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>({ defaultValues: { email: '', password: '', rememberMe: false } })
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    //dispatch(loginTC(data))
+    reset()
+  }
+
   return (
-    <h1>LOGIN</h1>
+    <Grid container justifyContent={'center'}>
+      <Grid item justifyContent={'center'}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: 'light', marginLeft: '5px' }}
+                href={'https://social-network.samuraijs.com/'}
+                target={'_blank'}
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormGroup>
+              <TextField
+                label="Email"
+                margin="normal"
+                {...register('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                    message: 'Incorrect email address',
+                  },
+                })}
+              />
+              {errors.email && <span className={s.errorMessage}>{errors.email.message}</span>}
+              <TextField
+                type="password"
+                label="Password"
+                margin="normal"
+                {...register('password', {
+                  required: 'Password is required',
+                  pattern: {
+                    value: /^.{3,}$/,
+                    message: 'Password must be at least 3 characters long',
+                  },
+                })}
+              />
+              {errors.password && <span className={s.errorMessage}>{errors.password.message}</span>}
+              <FormControlLabel
+                label={'Remember me'}
+                control={
+                  <Controller
+                    name={'rememberMe'}
+                    control={control}
+                    render={({ field: { value, ...rest } }) => <Checkbox {...rest} checked={value} />}
+                  />
+                }
+              />
+              <Button type={'submit'} variant={'contained'} color={'primary'}>
+                Login
+              </Button>
+            </FormGroup>
+          </form>
+        </FormControl>
+      </Grid>
+    </Grid>
   )
 }
